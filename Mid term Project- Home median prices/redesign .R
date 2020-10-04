6f27b172e623dda63afa19b4c5255f8e46957170
KausikValeti <72359776+KausikValeti@users.noreply.github.com>
2020-10-04 17:58:30 -0400

    Add files via upload

---
+++ Mid term Project- Home median prices/redesign .R
@@ -0,0 +1,39 @@
+# Group 8 - Sarah Hayes, Sean Lei, Kausik Valeti
+# STAT 515 - Applied Statistics and Visualization for Analytics
+# Redesign Project - The Most (and Least) Valuable States in America
+
+# Import required packages
+library(micromapST)
+library(tidyverse)
+
+# Read in dataset
+dataset = read.csv(
+  file="value of states.csv",
+  header=T, as.is=TRUE)
+colnames(dataset) = c("State", "Value per acre ($)", "Total value ($ bln)", "Total acres (mln)", "GDP per capita ($)", "Median home value ($)")
+head(dataset)
+
+# Panel description for linked micromap
+panelDesc = data.frame(
+  type=c('maptail','id','dot','dot'),
+  lab1=c(NA,NA,'Median Housing Price ($)','Value Per Acre ($)'),
+  lab2=c(NA,NA,' ',' '),
+  col1=c(NA,NA,'Median home value ($)','Value per acre ($)')
+  )
+
+# Create linked micromap
+fName = "homeValues.pdf"
+pdf(file=fName,width=7.5,height=10)
+micromapST(dataset, panelDesc,
+           rowNamesCol='State',
+           rowNames='full',
+           plotNames='full',
+           sortVar='Median home value ($)',
+           ascend=FALSE,
+           title=c("The Most (and Least) Valuable States in America","Median Housing Prices and Land Valuations Across the U.S."),
+           ignoreNoMatches=TRUE,
+           axisScale="sn",
+           staggerLab = TRUE
+)
+
+dev.off()
